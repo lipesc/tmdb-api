98ed52756de073d6c1697609f06f0385602355a2
lipesc <felipedrhouse@hotmail.com>
2025-02-03 20:28:17 -0300

    add constantes e implementação da função buscar top filmes

--- frontvue/src/App.vue
+++ frontvue/src/App.vue
@@ -1,10 +1,27 @@
 <script setup>
+import { ref, watchEffect } from 'vue';
 import MovieList from './components/MovieList.vue'
+
+const movies = ref([]);
+const currentPage = ref(1);
+const searchResults = ref([]);
+const searchQuery = ref("");
+
+const topMovies = async (page) => {
+  try {
+    const response = await fetch(`/movies/top/${page}`);
+    const data = await response.json();
+    movies.value = data.results;
+  } catch (error) {
+    console.error("erro ao buscar filmes: ", error);
+  }
+}
+
 </script>
 
 <template>
   
-  <MovieList :movies=""/>
+
 </template>
 
 <style scoped>
