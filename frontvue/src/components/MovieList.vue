5fb71abe9d74e2dc030397e5b9bfc070b69f9589
lipesc <felipedrhouse@hotmail.com>
2025-02-03 14:34:57 -0300

    componente MovieList, bloco de template implementado a logica para renderizar vai ser implentado no props movies do tipo array no App

--- frontvue/src/components/MovieList.vue
+++ frontvue/src/components/MovieList.vue
@@ -9,7 +9,14 @@ defineProps({
 </script>
 
 <template>
-
+  <div v-for="movie in movies" :key="movie.id" class="movie">
+    <img :src="'https://image.tmdb.org/t/p/original' + movie.poster_path" alt="poster movie" />
+    <h3>Filme: {{ movie.title }}</h3>
+    <p>Titulo original: {{ movie.original_title }}</p>
+    <p>Resumo: {{ movie.overview }}</p>
+    <p>Lançamento: {{ movie.release_date }}</p>
+    <p>Nota: {{ movie.vote_average }}</p>
+  </div>
 </template>
 
 <style scoped>
